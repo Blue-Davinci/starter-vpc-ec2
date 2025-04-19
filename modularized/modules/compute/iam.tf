5d09a0851e17268d841dc112c7af8548a30edc3e
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2025-04-19 16:29:32 +0300

    RAEMOTE CONNECTION NOW FULLY ALLOWED. Acess the remote instances via AWS SSM rather than SSH.

---
+++ modularized/modules/compute/iam.tf
@@ -0,0 +1,25 @@
+# Create an IAM role which will allow a service (ec2 instance) to assume the role and perform actions on behalf of the user.
+resource "aws_iam_role" "starter-vpc-ec2-ssm-role" {
+  name = "${var.tags["Project"]}-${var.tags["Environment"]}-ec2-ssm-role-${random_string.launch_template.result}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid = ""
+      }
+    ]
+  })
+  tags = merge(var.tags, { Name = "starter-vpc-ec2-ssm-role" })
+}
+
+# Attach the AmazonSSMManagedInstanceCore policy to the role allowing 
+# our ec2 instance to communicate with SSM (AWS Systems Manager)
+resource "aws_iam_role_policy_attachment" "ssm-ec2-attach" {
+  role       = aws_iam_role.starter-vpc-ec2-ssm-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
